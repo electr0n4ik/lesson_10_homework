51dbc06593af842656769b449c460491bad1b93b
electr0n4ik <andreyshka3@gmail.com>
2022-12-11 18:36:33 +0300

    Upgraded file: added comments every function

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 # 1
 def load_candidates():
+    """
+    ЗАгрузка списка кандитатов из файла
+    :return: возвращаем полученный список кандидатов
+    """
     import json
 
     with open("candidates.json", encoding="utf-8") as file:
@@ -9,12 +13,19 @@ def load_candidates():
 
 # 2
 def get_all():
-
+    """
+    :return: возвращаем готовый список кандидатов с помощью функции load_candidates
+    """
     return load_candidates()
 
 
 # 3
 def get_by_pk(pk):
+    """
+    Получает номер кандидата и возвращает кандидата с этим номером
+    :param pk: номер кандидата
+    :return: кандидат под номером pk
+    """
     for candidate in load_candidates():
         if pk == candidate["pk"]:
             return candidate
@@ -22,10 +33,14 @@ def get_by_pk(pk):
 
 # 4
 def get_by_skill(skill_name):
+    """
+    Получает навык и возвращает список кандидатов, с данным навыком
+    :param skill_name: навык для отбора кандидатов
+    :return: список кандидатов с определенным навыком
+    """
     list_candidates = []
     for candidate in load_candidates():
         skills = candidate["skills"].split(", ")
-
         for skill in skills:
             if skill_name == skill:
                 list_candidates.append(candidate)
